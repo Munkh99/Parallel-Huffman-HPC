e01e44269433d5f062d8517f76ccfc0c4c094829
Munkhdelger Bayanjargal <68891017+Munkh99@users.noreply.github.com>
2024-07-06 17:00:56 +0200

    Update huffman_encoding.sh

--- huffman_encoding.sh
+++ huffman_encoding.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=4:mem=2gb -l place=scatter
+#PBS -l select=1:ncpus=64:mem=2gb -l place=pack
 
-#PBS -l walltime=0:10:00
+#PBS -l walltime=0:30:00
 
 #PBS -q short_cpuQ
 
 module load mpich-3.2
-mpirun.actual -n 1 ./huffman_encoding random_text_1gb.txt
-mpirun.actual -n 1 ./huffman_encoding random_text_1gb.txt
-mpirun.actual -n 1 ./huffman_encoding random_text_1gb.txt
-mpirun.actual -n 1 ./huffman_encoding random_text_1gb.txt
-mpirun.actual -n 1 ./huffman_encoding random_text_1gb.txt
-mpirun.actual -n 1 ./huffman_encoding random_text_1gb.txt
-mpirun.actual -n 1 ./huffman_encoding random_text_1gb.txt
-mpirun.actual -n 1 ./huffman_encoding random_text_1gb.txt
-mpirun.actual -n 1 ./huffman_encoding random_text_1gb.txt
-mpirun.actual -n 1 ./huffman_encoding random_text_1gb.txt
-
-
-
+mpirun.actual -n 1 ./huffman_encoding file_2048M.txt
