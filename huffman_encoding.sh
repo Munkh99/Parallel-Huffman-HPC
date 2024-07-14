c8ef7f0b682aafffa7f4df2eb6740519eabb0cf6
Munkhdelger Bayanjargal <68891017+Munkh99@users.noreply.github.com>
2024-07-14 20:19:40 +0200

    Update huffman_encoding.sh

--- huffman_encoding.sh
+++ huffman_encoding.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#PBS -l select=1:ncpus=64:mem=2gb -l place=pack
+#PBS -l select=1:ncpus=32:mem=2gb -l place=pack
 
-#PBS -l walltime=0:30:00
+#PBS -l walltime=3:00:00
 
 #PBS -q short_cpuQ
 
 module load mpich-3.2
-mpirun.actual -n 1 ./huffman_encoding file_2048M.txt
+
+mpirun.actual -n 32 ./huffman_encoding file_512M.txt
